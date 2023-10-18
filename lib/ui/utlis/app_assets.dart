861ac7902ce1efd2fcb60880a36304a6eb0e7a20
khvtvb <mohamed.g.magdi@gmail.com>
2023-10-18 14:27:45 +0300

    utlis added app_assets & app_color

--- lib/ui/utlis/app_assets.dart
+++ lib/ui/utlis/app_assets.dart
@@ -1,1 +1,11 @@
-abstract class AppAssets {}
+abstract class AppAssets {
+  static const baseImagePath = "assets/images/";
+  static const icQuran = "${baseImagePath}ic_quran.png";
+  static const icAhadeth = "${baseImagePath}ic_ahadeth.png";
+  static const icRadio = "${baseImagePath}ic_radio.png";
+  static const icSebha = "${baseImagePath}ic_sebha.png";
+  static const ahadethTabLogo = "${baseImagePath}ahadeth_tab_logo.png";
+  static const quranTabLogo = "${baseImagePath}quran_tab_logo.png";
+  static const radioTabLogo = "${baseImagePath}radio_tab_logo.png";
+  static const sebhaTabLogo = "${baseImagePath}sebha_tab_logo.png";
+}
