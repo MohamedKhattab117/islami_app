5f1ce4642925ecc5199aad36bd894b2d22a92f89
khvtvb <mohamed.g.magdi@gmail.com>
2023-10-20 12:18:03 +0300

    file content

--- lib/ui/screens/details_screen/details_screen.dart
+++ lib/ui/screens/details_screen/details_screen.dart
@@ -1,11 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami_app/models/details_screen_args.dart';
+import 'package:islami_app/ui/utlis/app_colors.dart';
+import 'package:islami_app/ui/utlis/app_thems.dart';
 
-class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
-
+// ignore: must_be_immutable
+class DetailsScreen extends StatefulWidget {
   static const routeName = "details_screen";
+
+  DetailsScreen({super.key});
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  late DetailsScreenArgs arguments;
+
+  String fileContent = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    arguments = ModalRoute.of(context)!.settings.arguments as DetailsScreenArgs;
+
+    if (fileContent.isEmpty) {
+      readFile();
+    }
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.png"),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Appcolors.transparent,
+        appBar: AppBar(
+          backgroundColor: Appcolors.transparent,
+          centerTitle: true,
+          elevation: 0,
+          title: Text(
+            arguments.suraOrHadethName,
+            style: AppTheme.appBarTitleTextStyle,
+          ),
+        ),
+        body: Center(
+          child: Text(fileContent),
+        ),
+      ),
+    );
+  }
+
+  void readFile() async {
+    String file =
+        await rootBundle.loadString("assets/files/quran/${arguments.fileName}");
+    fileContent = file;
+    print(file);
+    setState(() {});
   }
 }
