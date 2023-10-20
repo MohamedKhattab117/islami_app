6081a42bbd65c84882c0e3bf5c3346468521951d
khvtvb <mohamed.g.magdi@gmail.com>
2023-10-20 10:02:18 +0300

    app bar, bottom navigation bar and background

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/ui/utlis/app_assets.dart';
+import 'package:islami_app/ui/utlis/app_colors.dart';
+import 'package:islami_app/ui/utlis/app_thems.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routName = "home";
@@ -6,6 +9,56 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.png"),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Appcolors.transparent,
+        appBar: AppBar(
+          backgroundColor: Appcolors.transparent,
+          centerTitle: true,
+          elevation: 0,
+          title: Text(
+            "Islami",
+            style: AppTheme.appBarTitleTextStyle,
+          ),
+        ),
+        bottomNavigationBar: buildBottomNavigationBar(),
+      ),
+    );
   }
 }
+
+Widget buildBottomNavigationBar() => Theme(
+      data: ThemeData(canvasColor: Appcolors.pramiry),
+      child: BottomNavigationBar(
+        selectedItemColor: Appcolors.accent,
+        iconSize: 30,
+        items: const [
+          BottomNavigationBarItem(
+            icon: ImageIcon(
+              AssetImage(AppAssets.icQuran),
+            ),
+            label: "Quran",
+          ),
+          BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(AppAssets.icAhadeth),
+              ),
+              label: "Ahadeath"),
+          BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(AppAssets.icSebha),
+              ),
+              label: "Sebha"),
+          BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(AppAssets.icRadio),
+              ),
+              label: "Radio"),
+        ],
+      ),
+    );
---
+++ lib/ui/utlis/app_thems.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/ui/utlis/app_colors.dart';
+
+abstract class AppTheme {
+  static const TextStyle appBarTitleTextStyle = (TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w700,
+    color: Appcolors.accent,
+  ));
+}
